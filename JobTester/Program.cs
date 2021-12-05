fbfc12a2085d23299e2286d0373771fed0309a42
yotakih <37173608+yotakih@users.noreply.github.com>
2021-12-06 02:23:44 +0900

    init

---
+++ JobTester/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+using SampleJob;
+
+namespace JobTester
+{
+    class Program
+    {
+        private readonly ILogger<Program> _logger;
+
+        public Program(ILogger<Program> logger)
+        {
+            this._logger = logger;
+        }
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().Build();
+            var logger = LogManager.Setup()
+                                   .SetupExtensions(ext => ext.RegisterConfigSettings(config))
+                                   .GetCurrentClassLogger();
+
+            try
+            {
+                var serviceProvider = BuildDi(config);
+                using (serviceProvider as IDisposable)
+                {
+                    logger.Info("start program!!");
+                    var program = serviceProvider.GetRequiredService<Program>();
+                    program.DoAction();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+            Console.ReadLine();
+        }
+
+        private static IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+                .AddTransient<Program>()
+                .AddLogging(builder => {
+                    builder.ClearProviders();
+                    builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    builder.AddNLog(config);
+                })
+                .BuildServiceProvider();
+        }
+
+        public void DoAction()
+        {
+            TestSampleJob();
+        }
+
+        private void TestSampleJob()
+        {
+            var cs = new CancellationTokenSource();
+            var smp = new SampleJobA(this._logger);
+            var tsk = smp.ExecuteAsyncTask(cs.Token);
+            _logger.LogInformation("Mainスレッドを2秒停止しますが、他スレッドには影響ありません");
+            Thread.Sleep(2000);
+
+            // _logger.LogInformation("キャンセルしますー");
+            // cs.Cancel();
+
+            _logger.LogInformation("失敗させますー");
+            smp.SetFail = true;
+
+            while (!(tsk.IsCanceled || tsk.IsCompleted || tsk.IsFaulted))
+            {
+                Thread.Sleep(1000);
+            }
+            _logger.LogInformation("Task Cancel");
+            _logger.LogInformation($"IsCanceled: {tsk.IsCanceled}");
+            _logger.LogInformation($"IsCompleted: {tsk.IsCompleted}");
+            _logger.LogInformation($"IsFaulted: {tsk.IsFaulted}");
+        }
+
+    }
+}
---
+++ SampleJob/SampleJobA.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace SampleJob
+{
+    public class SampleJobA
+    {
+        public bool SetFail = false;
+
+        private readonly ILogger _logger;
+
+        public SampleJobA(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task ExecuteAsyncTask(CancellationToken _cancelToken)
+        {
+            try
+            {
+                await Task.Run(()=>{
+                    ExecuteJob(_cancelToken);
+                }, _cancelToken);
+            }
+            catch(OperationCanceledException e)
+            {
+                _logger.LogError($"処理がキャンセルされました");
+            }
+            catch(Exception e)
+            {
+                _logger.LogError($"{e.Message}");
+                throw;
+            }
+            finally
+            {
+                //データベースなどのセッションを閉じる
+                _logger.LogInformation("SampleTaskA exec finally");
+            }
+        }
+
+        private void ExecuteJob(CancellationToken _cancelToken)
+        {
+            // hevy job
+            for(var i=0;i<5;i++)
+            {
+                //処理がキャンセルされた場合、キャンセル例外をスロー
+                _cancelToken.ThrowIfCancellationRequested();
+                if (this.SetFail) throw new Exception("Set Fail Execption");
+                _logger.LogInformation("SampleJob Execute!!");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
